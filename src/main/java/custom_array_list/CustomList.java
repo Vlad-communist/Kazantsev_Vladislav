2b9636150590dc9e150c2dde5bc57a745a736ad4
Хмелёв Максим <kazantsev.vi@phystech.edu>
2024-10-06 19:05:39 +0300

    added solution to 2nd hw

--- src/main/java/custom_array_list/CustomList.java
+++ src/main/java/custom_array_list/CustomList.java
@@ -1,2 +1,26 @@
-package custom_array_list;public class CustomList {
+package custom_array_list;
+
+/**
+ * Интерфейс, описывающий базовые методы.
+ * @param <A> Тип элементов, которые будут храниться в списке.
+ */
+public interface CustomList<A> {
+    /**
+     * Добавляет элемент в конец списка.
+     * @param element Элемент для добавления.
+     */
+    void add(A element);
+
+    /**
+     * Получает элемент по указанному индексу.
+     * @param index Индекс элемента.
+     * @return Элемент на указанном индексе.
+     */
+    A get(int index);
+
+    /**
+     * Удаляет элемент по индексу.
+     * @param index Индекс элемента.
+     */
+    void remove(int index);
 }
